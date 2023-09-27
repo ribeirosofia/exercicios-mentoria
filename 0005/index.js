07aefe7560238a5946c6fc098371c2b82a5eccad
ribeirosofia <sofia.ribeiro@creditas.com>
2023-09-27 14:52:14 -0300

    feat: create exercise 0005

--- 0005/index.js
+++ 0005/index.js
@@ -1,3 +1,19 @@
-// 5 - Faça um programa que receba a idade de dez 
-// pessoas e que calcule e mostre a quantidade 
+// 5 - Faça um programa que receba a idade de dez
+// pessoas e que calcule e mostre a quantidade
 // de pessoas com idade maior ou igual a 18 anos.
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+let quantidade = 0;
+
+for (let i = 0; i <= 10; i++) {
+  let idade = parseInt(prompt("Insira uma idade: "));
+
+  if (idade >= 18) {
+    quantidade++;
+  }
+}
+
+console.log(
+  `Quantidade de pessoas com idade maior ou igual a 18 anos: ${quantidade}`
+);
