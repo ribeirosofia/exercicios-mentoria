477934526232447ff8925091398f1a8ab7c522e6
ribeirosofia <sofia.ribeiro@creditas.com>
2023-10-03 12:27:38 -0300

    feat: resolution of exercise 0006

--- 0006/index.js
+++ 0006/index.js
@@ -1,5 +1,5 @@
-// 6 - Faça um programa que recebe a altura de um 
-// triangulo em um número inteiro e imprima-o 
+// 6 - Faça um programa que recebe a altura de um
+// triangulo em um número inteiro e imprima-o
 // utilizando asteriscos. Veja o Exemplo:
 
 // Entrada: 5
@@ -9,3 +9,12 @@
 // ***
 // ****
 // *****
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+let altura = parseInt(prompt("Insira uma altura: "));
+
+for (let i = 0; i < altura; i++) {
+  let linha = "*".repeat(i);
+  console.log(linha);
+}
