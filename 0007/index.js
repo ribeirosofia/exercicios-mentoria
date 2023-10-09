743802eadd521d59f33709b459c3e5a57bece813
ribeirosofia <sofia.ribeiro@creditas.com>
2023-10-09 17:00:05 -0300

    feat: exercise 0007

--- 0007/index.js
+++ 0007/index.js
@@ -1,1 +1,11 @@
 // 7 - Faça uma função que ordene uma lista de números.
+
+const listOfNumbers = (list) => {
+  list.sort((a, b) => {
+    return a - b;
+  });
+  return list;
+};
+
+const numbers = [89, 665, 355, 456, 47];
+console.log(listOfNumbers(numbers));
